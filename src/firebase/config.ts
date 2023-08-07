332e6514ce662ffd6105b50b229b581814a5b1cb
duyphan119 <duy.910.2015@gmail.com>
2023-08-07 21:12:19 +0700

    update firebase 2

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -9,6 +9,6 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
-
+console.log(firebaseConfig);
 const app = initializeApp(firebaseConfig);
 getAnalytics(app);
